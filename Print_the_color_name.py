f75586fa5364408b679c7747bfc9d45a0c5d7f5b
Spandana.vankayala <119720503+22A91A04P6@users.noreply.github.com>
2023-08-18 15:05:55 +0530

    ch=input() if ch=="B":     print("Blue") elif ch=="G":     print("Green") elif ch=="I":     print("Indigo") elif ch=="O":     print("Orange") elif ch=="V":     print("Violet") elif ch=="R":     print("Red") elif ch=="Y":     print("Yellow") else:     print(-1)

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+ch=input()
+if ch=="B":
+    print("Blue")
+elif ch=="G":
+    print("Green")
+elif ch=="I":
+    print("Indigo")
+elif ch=="O":
+    print("Orange")
+elif ch=="V":
+    print("Violet")
+elif ch=="R":
+    print("Red")
+elif ch=="Y":
+    print("Yellow")
+else:
+    print(-1)
